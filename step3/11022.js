3f01721a2ea5bd3c2c77498cc1b0bb26020888c3
duboo <duboo31@gmail.com>
2021-11-17 23:07:55 +0900

    11022.js

---
+++ step3/11022.js
@@ -0,0 +1,10 @@
+const [n, ...arr] = require('fs').readFileSync('../example.txt').toString().trim().split('\n');
+
+let numArr = arr.map(el => el.split(' ').map(Number));
+
+let answer = '';
+
+for(let i=0; i<+n; i++) {
+    answer += `Case #${i + 1}: ${numArr[i][0]} + ${numArr[i][1]} = ${numArr[i][0] + numArr[i][1]}${'\n'}`;
+}
+console.log(answer);

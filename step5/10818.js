7712ad21ecb0af7dad024ec1e4c82827a7c1b55c
duboo <duboo31@gmail.com>
2021-11-19 03:58:10 +0900

    completed 10818.js

---
+++ step5/10818.js
@@ -0,0 +1,6 @@
+let [n, ...arr] = require('fs').readFileSync('../example.txt').toString().trim().split(/\s+/);
+
+arr = arr.map(Number).sort((a, b) => a - b);
+
+console.log(arr[0], arr[arr.length - 1]);
+

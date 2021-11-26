93efba03da6c2df44c076166076948d75650dec4
duboo <duboo31@gmail.com>
2021-11-26 14:36:18 +0900

    completed 2839.js

--- step8/2839.js
+++ step8/2839.js
@@ -1,18 +1,17 @@
 let n = require('fs').readFileSync('../example.txt').toString();
 
 let sugar = +n;
-let cnt = 0;
+let bag = 0;
 
 while(true) {
     if(sugar % 5 === 0) {
-        console.log(sugar / 5 + cnt);
+        console.log(sugar / 5 + bag);
         break;
     } else if(sugar <= 0) {
         console.log(-1);
         break;
     } else {
         sugar -= 3;
-        cnt++;
+        bag++;
     }
-
 }

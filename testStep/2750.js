3fb95f1c5aa440d660dd73e2dda96eaf04049d3b
duboo <duboo31@gmail.com>
2021-11-27 23:06:04 +0900

    completed 2750.js

---
+++ testStep/2750.js
@@ -0,0 +1,7 @@
+let [n, ...arr] = require('fs').readFileSync('../example.txt').toString().trim().split('\n').map(Number);
+
+arr = arr.sort((a, b) => a - b);
+
+for(let i=0; i<n; i++) {
+    console.log(arr[i]);
+}

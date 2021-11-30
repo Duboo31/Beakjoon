c4fd250562f067cd2c0e2337620fb4606a10fd38
duboo <duboo31@gmail.com>
2021-11-30 14:34:07 +0900

    fix 10757.js

--- step8/10757.js
+++ step8/10757.js
@@ -3,4 +3,6 @@ let [a, b] = require('fs').readFileSync('../example.txt').toString().split(' ');
 a = BigInt(a);
 b = BigInt(b);
 
-console.log(Stirng(a + b));
+let answer = a + b;
+
+console.log(answer.toString())

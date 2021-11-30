9b10ace0975ceaab800ffb32dce1311632e7d4cc
duboo <duboo31@gmail.com>
2021-11-30 20:03:44 +0900

    fix 10250.js

--- step8/10250.js
+++ step8/10250.js
@@ -6,4 +6,4 @@ for(let i=0; i<+n; i++) {
     let floor = HWN[2] % HWN[0] === 0 ? HWN[0] : HWN[2] % HWN[0]; 
     let roomNum = Math.ceil(HWN[2] / HWN[0]) < 10 ? '0' + Math.ceil(HWN[2] / HWN[0]) : Math.ceil(HWN[2] / HWN[0]);
     console.log(`${floor}${roomNum}`);
-}
+}    

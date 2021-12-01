dae3d09aaa891a78f8288b6904de2729f21d29bc
duboo <duboo31@gmail.com>
2021-12-01 19:15:57 +0900

    completed but not wort cus require('fs')

--- step8/1011.js
+++ step8/1011.js
@@ -1,3 +1,20 @@
 let [n, ...arr] = require('fs').readFileSync('../example.txt').toString().split('\n');
 
 arr = arr.map(el => el.split(' ').map(Number).reduce((acc, cur) => (cur - acc)));
+
+let cnt = 0;
+
+for(let i=0; i<+n; i++) {
+    while(true) {
+        if(arr[i] <= Math.pow(cnt, 2) + 1) {
+            break;
+        } else {
+            cnt += 1;
+        }
+    }
+    if(arr[i] <= Math.pow(cnt, 2)) {
+        console.log(cnt * 2 - 1);
+    } else {
+        console.log(cnt * 2);
+    }
+}

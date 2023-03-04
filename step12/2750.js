78ac21d40773c803bd0dee9cff4b38384c07c023
hyun <duboo31@gmail.com>
2023-03-04 21:06:22 +0900

    test clon

--- step12/2750.js
+++ step12/2750.js
@@ -1,7 +1,12 @@
-let [n, ...arr] = require('fs').readFileSync('../example.txt').toString().trim().split('\n').map(Number);
+let [n, ...arr] = require("fs")
+  .readFileSync("../example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
 
 arr = arr.sort((a, b) => a - b);
 
-for(let i=0; i<n; i++) {
-    console.log(arr[i]);
+for (let i = 0; i < n; i++) {
+  console.log(arr[i]);
 }
